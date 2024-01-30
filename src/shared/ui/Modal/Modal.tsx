70ae8d78509f9c02781857991d9fd86c36b248ef
Sofia <vdo-sofia@mail.ru>
2024-01-30 16:26:00 +0300

    fix theme for modal window

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -9,6 +9,8 @@ import React, {
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Modal.module.scss';
 import { Portal } from '../Portal/Portal';
+import { useTheme } from 'app/providers/ThemeProvider';
+
 
 interface ModalProps {
   className?: string;
@@ -28,6 +30,7 @@ export const Modal: FC<ModalProps> = (props) => {
   } = props;
   const [isClosing, setIsClosing] = useState(false);
   const timerRef = useRef<ReturnType<typeof setTimeout>>();
+  const { theme } = useTheme();
 
   const closeHandler = useCallback(() => {
     if (onClose) {
@@ -67,7 +70,7 @@ export const Modal: FC<ModalProps> = (props) => {
 
   return (
     <Portal>
-      <div className={classNames(cls.Modal, mods, [className])}>
+      <div className={classNames(cls.Modal, mods, [className, theme])}>
         <div className={cls.overlay} onClick={closeHandler}>
           <div
             className={cls.content}
